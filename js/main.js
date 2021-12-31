701c4caeda028acbec160572e4d874a6ded1867c
Ararat2 <araratm@boomte.ch>
2021-12-31 22:22:56 +0400

    Index page is done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 "use strict";
 
+const SLIDER_AUTOPLAY_SPEED = 1500;
+
 /* - Main Slider - */
-$(".main-slider-wrapper .slider").slick({
+$(".main-slider").slick({
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 1500,
+    autoplaySpeed: SLIDER_AUTOPLAY_SPEED,
+});
+
+/* - Products Slider - */
+$(".products-slider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: SLIDER_AUTOPLAY_SPEED,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
 });
